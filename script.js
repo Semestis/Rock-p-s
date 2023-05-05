30bf08a5ff62d3605faaf44c372e4592c0b103a4
Semesta <{ID}+{username}@users.noreply.github.com>
2023-05-05 21:51:12 +0200

    Add choice Player / Comp

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 
+
+
+function playRound(playerSelection, computerSelection) {
+
+
+
+
+
+
+
+  
+  
+ function playerSelection () {
+
+  let playerString = prompt("Rock, paper, scissors?");
+  const playerCase = playerString.toUpperCase();
+
+  if (playerCase == "ROCK") {
+
+    return "Rock";
+  } if (playerCase == "PAPER") {
+    return "Paper";
+  } if (playerCase == "SCISSORS") {
+    return "Scissors";
+  }
+
+ } 
+
+
+  
+  
+function getComputerChoice () {
+  
+  const computerString = ("hot");
+  const computerSelection = Math.floor(Math.random() * computerString.length + 1);
+
+  if (computerSelection == 1) {
+    return "Rock";
+  } if (computerSelection == 2) { 
+    return "Paper";
+  } if (computerSelection == 3) { 
+    return "Scissors";
+  }
+}
+ 
+
+  
+  
+  
+  
+  
+
+}
+
+  function game () {
+
+  }
