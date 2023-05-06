17e2126bf401c174957c461aca6f3542533cab02
Semesta <{ID}+{username}@users.noreply.github.com>
2023-05-06 02:21:56 +0200

    Run game no score

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 
 
 
-function playRound(playerSelection, computerSelection) {
 
 
 
 
 
 
+function playRound(playerSelection, computerSelection, getWinner) {
 
   
-  
+
+
+
  function getPlayerSelection () {
 
-  let playerString = prompt("Rock, paper, scissors?");
+  let playerString = prompt("Rock, paper or scissors?");
   const playerCase = playerString.toUpperCase();
 
   if (playerCase == "ROCK") {
@@ -23,14 +25,14 @@ function playRound(playerSelection, computerSelection) {
     return "Paper";
   } if (playerCase == "SCISSORS") {
     return "Scissors";
+  } else if (playerCase != true) {
+    getPlayerSelection();
   }
-
  } 
 
-
   
   
-function getComputerChoice () {
+  function getComputerChoice () {
   
   const computerString = ("hot");
   const computerSelection = Math.floor(Math.random() * computerString.length + 1);
@@ -47,16 +49,71 @@ function getComputerChoice () {
 
 
 
-const computer = getComputerChoice();
-const player = getPlayerSelection();
+  function getWinner() {
+
+    if ((computer == "Rock") && (player == "Rock")) {
+      return "It's a tie!";
+    } else if ((computer == "Rock") && (player == "Paper")) {
+      return "You won!";
+    } else if ((computer == "Rock") && (player == "Scissors")) {
+      return "The Computer won!";
+    } 
+    if ((computer == "Paper") && (player == "Rock")) {
+      return "The Computer won!";    
+    } else if ((computer == "Paper") && (player == "Paper")) {
+      return "It's a tie!";
+    } else if ((computer == "Paper") && (player == "Scissors")) {
+      return "You won!";
+    } 
+    if ((computer == "Scissors") && (player == "Rock")) {
+      return "You won!";     
+    } else if ((computer == "Scissors") && (player == "Paper")) {
+      return "The Computer won!";
+    } else if ((computer == "Scissors") && (player == "Scissors")) {
+      return "It's a tie!";
+    } 
+   
+  }
+
+  const computer = getComputerChoice();
+  const player = getPlayerSelection();
+  const winner = getWinner();
   
 
   
   
-console.log("You chose " + player + "!", "\nThe computer chose " + computer + "!");
+  console.log("You chose " + player + "!", "\nThe computer chose " + computer + "!\n\n" + winner);
+
+
 
 }
 
-  function game () {
+function game () {
 
-  }
+  let a = 0;
+  let b = 0;
+  let c = 0;
+  const e = a + 1;
+  const r = b + 1;
+
+  playRound();
+
+
+
+  if (`getWinner() == "You won!") {
+    
+    
+    return "You have " + e +  " point(s)";
+
+   } else if (`getWinner() === "The Computer won!") {
+    return 100;
+    return "Computer have " + r + " point(s)"
+   } else if (`getWinner() === "It's a tie!"`) {
+    return a, b;
+
+   
+   }
+
+
+
+}
